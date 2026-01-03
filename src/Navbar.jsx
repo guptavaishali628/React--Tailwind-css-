768a2524ebfc3c8a9fd9c6d1e9dcb24d02808835
vaishali <guptavaishali628@gmail.com>
2026-01-03 18:26:38 +0530

    show data using map function

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './Tailwind.css'
 
 const Navbar = () => {
+  
+  let Fruits=['Apple', 'Mango', 'Banana', 'Grapes']
+
   return (
     <>
       <header className=' w-full h-20 border flex justify-between items-center pl-10 pr-10'>
@@ -16,6 +19,14 @@ const Navbar = () => {
         </nav>
         <button className='border p-2 rounded-2x1 bg-cyan-500 hover:bg-amber-400'>Login</button>
       </header>
+      <ul>
+        {
+          Fruits.map((e,idx)=>(
+            // return e
+            <li key={idx}>{e}</li>
+          ))
+        }
+      </ul>
 
       {/* by default - mobile
       sm=640px - 768px tablet
